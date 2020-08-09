570bea45d139b7000e9889d40daa851106e4506b
Alexander <adedood@gmail.com>
2020-08-08 17:47:20 -0700

    Impliment players adding themselves to the game

--- src/me/thebereavingbee/Juggernaut/Main.java
+++ src/me/thebereavingbee/Juggernaut/Main.java
@@ -9,7 +9,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+ArrayList<Player> joined = new ArrayList<Player>();
+	
 		@Override
 		public void onEnable() {
 			
@@ -24,33 +26,50 @@ public class Main extends JavaPlugin {
 		
 		
 		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-			ArrayList<Player> joined = new ArrayList<Player>();
 			
 			if (label.equalsIgnoreCase("juggernaut")) {
 				if (sender instanceof Player) {
 					// player
 					Player player = (Player) sender;
-					if (args[0] == "start") {
-						if (player.hasPermission("juggernaut.start")) {
-							//  start the game
-							if (!joined.isEmpty()) {
-								Player theJugg = randomPlayer((Player[])joined.toArray());
-								
-								createJuggernaut(theJugg);
+					if (args.length >= 1) {	
+						if (args[0].equalsIgnoreCase("start")) {
+							if (player.hasPermission("juggernaut.start")) {
+								//  start the game
+								if (joined.size() >= 1) {
+									//starts game if there are people in list
+									Player theJugg = randomPlayer((Player[])joined.toArray());
+									createJuggernaut(theJugg);
+								}
+								else {
+									player.sendMessage("Nobody is on the list to play! They can join with /juggernaut");
+								}
 							}
 							else {
-								player.sendMessage("Nobody is on the list to play! They can join with /juggernaut");
+								player.sendMessage("You do not have permission to start the game!");
+							}
+						}
+						else if (args[0].equalsIgnoreCase("list")) {
+							if (player.hasPermission("juggernaut.list")) {
+								// /juggernaut list
+								// prints list of players in game
+								player.sendMessage("Current Players in game:");
+								for (int i = 0; i < joined.size(); i++) {
+									player.sendMessage(joined.get(i).getName());
+								}
+							}
+							else {
+								player.sendMessage("You dont have permission to list players in game!");
 							}
 						}
 						else {
-							player.sendMessage("You do not have permission to start the game!");
+							player.sendMessage("Usage: /juggernaut <list|start>");
 						}
-					}
-					else {
+					}else {
 						if (player.hasPermission("juggernaut.join")) {
 							// add to game queue
 							joined.add(player);
 							player.sendMessage("You have been added to the game!");
+							player.sendMessage("Players ready: " + String.valueOf(joined.size()));
 						}
 					}
 				}
